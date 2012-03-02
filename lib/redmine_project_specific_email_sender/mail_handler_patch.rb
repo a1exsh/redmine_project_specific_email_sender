ec7d8ec9a801196cfa94ee330f21b8741653e37b
Alex Shulgin <ash@commandprompt.com>
2012-03-02 16:28:32 +0200

    Fixup.

--- lib/redmine_project_specific_email_sender/mail_handler_patch.rb
+++ lib/redmine_project_specific_email_sender/mail_handler_patch.rb
@@ -10,7 +10,12 @@ module RedmineProjectSpecificEmailSender
     
     module InstanceMethods
       def receive_issue_with_project_specific_email
-        receive_issue_without_project_specific_email if @email.to_addrs.map(&:spec).include? target_project.email
+        if @email.to_addrs.map(&:spec).include? target_project.email
+          receive_issue_without_project_specific_email
+        else
+          logger.info "Not creating ticket as project email is not in To: header" if logger
+          true # so the controller returns success status
+        end
       end
     end
   end
